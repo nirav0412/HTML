5283a58ce5282b9d0d6d3e4942d71f204e1f989a
Nirav Katharotiya <kathrotiyanirav9@gmail.com>
2022-12-25 21:36:56 +0530

    Add files via upload
    initial commit

---
+++ 1.js
@@ -0,0 +1,51 @@
+console.log("hello world");
+console.warn("this is warning");
+console.error("this is an error");
+//Data Types
+///numbers
+var number1 = 34;
+var number2 = 66;
+console.log(number1 + number2); 
+//strings
+var str1 = "this is a string";
+var str2 = 'this is also a string';
+//objects
+var marks = {
+    ravi:69,
+    abhi:99,
+}
+console.log(marks);
+//booleans
+ var a = true;
+ var b = false;
+console.log(a, b);
+
+var n = null;
+console.log(n);
+
+//var arr = [1,2,3,4,5];
+//console.log(arr);
+
+function avg(a,b){
+    return (a + b)/2;
+}
+ c = avg(45,69);
+ console.log(c);
+
+ var age = 34;
+ if(age>=18){
+     console.log("you can vote");
+ }
+
+  //var arr = [1,2,3,4,5];
+ //for(var i=0;i<=arr.length;i++){
+     //console.log(i);
+ //}
+
+ //let j = 0;
+  //while(j<arr.length){
+      //console.log(arr[j]);
+      //j++;
+  //}
+      let arr = [ "fan", "camera", 23, 4, null, true]
+      console.length(myarr.length);
